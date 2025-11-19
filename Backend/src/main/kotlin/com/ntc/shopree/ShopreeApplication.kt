fd602d13dd70d2bbf2885bdc90bd085590d330cd
cuongnt02 <cuong.ntc02@gmail.com>
2025-11-19 13:06:36 +0700

    [SH-1] add backend

---
+++ Backend/src/main/kotlin/com/ntc/shopree/ShopreeApplication.kt
@@ -0,0 +1,11 @@
+package com.ntc.shopree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShopreeApplication>(*args)
+}
---
+++ Backend/src/main/kotlin/com/ntc/shopree/model/Product.kt
@@ -0,0 +1,57 @@
+package com.ntc.shopree.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import java.time.Instant
+import java.util.UUID
+
+
+@Entity
+class Product (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UUID,
+
+
+    @ManyToOne
+    @JoinColumn(name = "vendor_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    var vendor: Vendor,
+
+    @Column(length = 255, nullable = false)
+    var title: String,
+
+    @Column(length = 255, unique = true)
+    var slug: String,
+
+    var description: String? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    var category: Category? = null,
+
+    @field:Column(length = 1024)
+    mainImage: String? = null,
+
+    var tags: MutableList<String>? = null,
+
+    var pickupAvailable: Boolean = false,
+
+    var createdAt: Instant = Instant.now(),
+
+    var updatedAt: Instant = Instant.now(),
+
+) {
+    enum class Status {
+        DRAFT, PUBLISHED, DISABLED, PENDING_APPROVAL
+    }
+}
