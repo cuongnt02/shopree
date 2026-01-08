74c19dded3001fc58068d76f33faa850fab80088
cuongnt02 <cuong.ntc02@gmail.com>
2026-01-08 15:15:57 +0700

    [SH-13] Aside - load products on catalog

--- MobileApp/core/ui/src/main/java/com/ntc/shopree/core/ui/components/ProductCard.kt
+++ MobileApp/core/ui/src/main/java/com/ntc/shopree/core/ui/components/ProductCard.kt
@@ -1,19 +1,15 @@
 package com.ntc.shopree.core.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -22,17 +18,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.AsyncImage
-import coil3.compose.AsyncImagePreviewHandler
-import coil3.compose.LocalAsyncImagePreviewHandler
 import com.ntc.shopree.core.ui.icons.Icons
 import com.ntc.shopree.core.ui.theme.ColorGrey100
 import com.ntc.shopree.core.ui.theme.ColorGrey200
@@ -41,15 +32,26 @@ import com.ntc.shopree.core.ui.theme.ColorGrey200
 fun ProductCard(image: @Composable () -> Unit, name: String, price: String) {
     return Card(
         colors = CardDefaults.cardColors(containerColor = ColorGrey200),
-        modifier = Modifier.width(173.dp).wrapContentHeight(),
+        modifier = Modifier
+            .width(173.dp)
+            .wrapContentHeight(),
         shape = RoundedCornerShape(25.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
-            Box(modifier = Modifier.align(Alignment.End).background(ColorGrey100, shape = CircleShape).padding(4.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .background(ColorGrey100, shape = CircleShape)
+                    .padding(4.dp)
+            ) {
                 Icon(imageVector = Icons.Outlined.Heart, contentDescription = null)
             }
             Spacer(modifier = Modifier.height(14.dp))
-            Box(modifier = Modifier.align(Alignment.CenterHorizontally).widthIn(max=140.dp)) {
+            Box(modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .widthIn(max = 140.dp)) {
                 image()
             }
             Spacer(modifier = Modifier.height(19.dp))
@@ -63,19 +65,11 @@ fun ProductCard(image: @Composable () -> Unit, name: String, price: String) {
 
 @Composable
 fun ProductCard(imageSource: String, name: String, price: String) {
-    return Card(
-        colors = CardDefaults.cardColors(containerColor = ColorGrey200),
-        modifier = Modifier.width(173.dp).wrapContentHeight(),
-        shape = RoundedCornerShape(25.dp)
-    ) {
-        ProductCard(
-            image = {
-                AsyncImage(model = imageSource, contentDescription = "product $name image")
-            },
-            name = name,
-            price = price
-        )
-    }
+    ProductCard(
+        image = {
+            AsyncImage(model = imageSource, contentDescription = "product $name image")
+        }, name = name, price = price
+    )
 }
 
 @OptIn(ExperimentalCoilApi::class)
