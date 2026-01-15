74641afa08b90039db70aa2940b33d673e3ff74e
cuongnt02 <cuong.ntc02@gmail.com>
2026-01-15 20:38:08 +0700

    [SH-14] Add domain use cases for cart icrement use case

---
+++ MobileApp/feature/cart/src/main/java/com/ntc/shopree/feature/cart/domain/IncrementCartItemUseCase.kt
@@ -0,0 +1,17 @@
+package com.ntc.shopree.feature.cart.domain
+
+import com.ntc.shopree.core.model.CartItem
+import javax.inject.Inject
+
+class IncrementCartItemUseCase @Inject constructor(
+    private val cartRepository: CartRepository
+) {
+    suspend operator fun invoke(item: CartItem): Result<Int> {
+        return try {
+            cartRepository.incrementQuantity(item)
+            Result.success(item.quantity)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
